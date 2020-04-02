b2b552215eec782e85b681026ceb1aa64bd4dbaf
romluc <romulofilhofilho@gmail.com>
2020-04-01 21:47:49 -0300

    Creates initial components for the markup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
+const ShowLaps = props => {
+  return (
+    <p>
+      {props.laps}
+      <br />
+      laps
+    </p>
+  );
+};
+
+const ShowTime = props => {
+  return (
+    <p>
+      {props.time}
+      <br />
+      Average time per lap
+    </p>
+  );
+};
+
+const Button = props => {
+  return <button>{props.text}</button>;
+};
+
 function App() {
   return (
     <div className='App'>
-      <p>
-        10
-        <br />
-        laps
-      </p>
-      <button>+</button>
-      <button>-</button>
-      <p>
-        01:30
-        <br />
-        Average time per lap
-      </p>
-      <button>Start</button>
-      <button>Restart</button>
+      <ShowLaps laps='15' />
+      <Button text='+' />
+      <Button text='-' />
+      <ShowTime time='01:30' />
+      <Button text='Start' />
+      <Button text='Restart' />
     </div>
   );
 }
