fe8678ef98f6fc2bec1d3aaec6cdfef84ef855df
romluc <romulofilhofilho@gmail.com>
2020-04-01 21:21:09 -0300

    Create initial hard-coded markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <p>
+        10
+        <br />
+        laps
+      </p>
+      <button>+</button>
+      <button>-</button>
+      <p>
+        01:30
+        <br />
+        Average time per lap
+      </p>
+      <button>Start</button>
+      <button>Restart</button>
+    </div>
+  );
 }
 
 export default App;
